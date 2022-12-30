a57d7c7a286dca6af849ced27d13553023c0ab0c
kajal1112lad <121658512+kajal1112lad@users.noreply.github.com>
2022-12-30 11:56:17 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int a,b,sum;
+	printf("\n enter a=");
+	scanf("%d",&a);
+	printf("\n enter b=");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("\n sum of two no=%d",sum);
+	
+}
